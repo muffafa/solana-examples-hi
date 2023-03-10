fa6703cd207e345874232f89ca94ce402d7f05ef
muffafa <mm.savar@gmail.com>
2023-03-10 23:56:58 +0300

    ping added

--- solana-ping-frontend/pages/_app.tsx
+++ solana-ping-frontend/pages/_app.tsx
@@ -25,7 +25,7 @@ const App = ({ Component, pageProps }) => {
   // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --// Only the wallets you configure here will be compiled into your application, and only the dependencies// of wallets that your users connect to will be loaded
 
   const wallets = useMemo(
-    () => [new PhantomWalletAdapter(), new GlowWalletAdapter()],
+    () => [new PhantomWalletAdapter()],
     [network]
   );
 
