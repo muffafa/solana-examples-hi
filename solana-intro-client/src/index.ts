cd1938ae3d216b83a71e39be2c6e3aa46148c755
muffafa <mm.savar@gmail.com>
2023-03-10 21:57:28 +0300

    send transaction added

--- solana-intro-client/src/index.ts
+++ solana-intro-client/src/index.ts
@@ -51,12 +51,17 @@ async function airdropSolIfNeeded(
   }
 }
 
-async function checkBalance(connection: Web3.Connection, signer: Web3.Keypair){
-    const balance = await connection.getBalance(signer.publicKey);
-    console.log("Current balance is", balance / Web3.LAMPORTS_PER_SOL, "SOL");
+async function checkBalance(connection: Web3.Connection, signer: Web3.Keypair) {
+  const balance = await connection.getBalance(signer.publicKey);
+  console.log("Current balance is", balance / Web3.LAMPORTS_PER_SOL, "SOL");
 }
 
-async function pingProgram(connection: Web3.Connection, payer: Web3.Keypair, PROGRAM_DATA_PUBLIC_KEY: Web3.PublicKey, PROGRAM_ID: Web3.PublicKey){
+async function pingProgram(
+  connection: Web3.Connection,
+  payer: Web3.Keypair,
+  PROGRAM_DATA_PUBLIC_KEY: Web3.PublicKey,
+  PROGRAM_ID: Web3.PublicKey
+) {
   const transaction = new Web3.Transaction();
   const instruction = new Web3.TransactionInstruction({
     // Instructions need 3 things
@@ -88,23 +93,53 @@ async function pingProgram(connection: Web3.Connection, payer: Web3.Keypair, PRO
   );
 }
 
+async function sendSol(
+  connection: Web3.Connection,
+  amount: number,
+  to: Web3.PublicKey,
+  sender: Web3.Keypair
+) {
+  const transaction = new Web3.Transaction();
+
+  const sendSolInstruction = Web3.SystemProgram.transfer({
+    fromPubkey: sender.publicKey,
+    toPubkey: to,
+    lamports: amount,
+  });
+
+  transaction.add(sendSolInstruction);
+
+  const sig = await Web3.sendAndConfirmTransaction(connection, transaction, [
+    sender,
+  ]);
+  console.log(
+    `You can view your transaction on the Solana Explorer at:\nhttps://explorer.solana.com/tx/${sig}?cluster=devnet`
+  );
+}
+
 async function main() {
   const connection = new Web3.Connection(Web3.clusterApiUrl("devnet"));
   const signer = await initializeKeypair(connection);
 
   console.log("Public key:", signer.publicKey.toBase58());
 
-  //if necessary get airdrop
-  await airdropSolIfNeeded(signer, connection);
+  // //if necessary get airdrop
+  // await airdropSolIfNeeded(signer, connection);
 
-  const PROGRAM_ID = new Web3.PublicKey(
-    "ChT1B39WKLS8qUrkLvFDXMhEJ4F1XZzwUNHUt4AU9aVa"
-  );
-  const PROGRAM_DATA_PUBLIC_KEY = new Web3.PublicKey(
-    "Ah9K7dQ8EHaZqcAsgBW8w37yN2eAy3koFmUn4x3CJtod"
-  );
+  // const PROGRAM_ID = new Web3.PublicKey(
+  //   "ChT1B39WKLS8qUrkLvFDXMhEJ4F1XZzwUNHUt4AU9aVa"
+  // );
+  // const PROGRAM_DATA_PUBLIC_KEY = new Web3.PublicKey(
+  //   "Ah9K7dQ8EHaZqcAsgBW8w37yN2eAy3koFmUn4x3CJtod"
+  // );
 
-  await pingProgram(connection, signer, PROGRAM_DATA_PUBLIC_KEY, PROGRAM_ID);
+  // await pingProgram(connection, signer, PROGRAM_DATA_PUBLIC_KEY, PROGRAM_ID);
+
+  const MY_PUBLIC_KEY = new Web3.PublicKey("2AYwCyUGr53qup83zV58vaXVkBZkrsa7iFKg5c5xgQLJ");
+
+  await checkBalance(connection, signer);
+
+  await sendSol(connection, 0.1*Web3.LAMPORTS_PER_SOL, MY_PUBLIC_KEY, signer);
 
   await checkBalance(connection, signer);
 }
