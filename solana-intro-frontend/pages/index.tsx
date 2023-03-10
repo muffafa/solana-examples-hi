23297d65dbc2a2cc9aa301746d18c53841c48342
muffafa <mm.savar@gmail.com>
2023-03-10 05:16:24 +0300

    is executable and try catch added

--- solana-intro-frontend/pages/index.tsx
+++ solana-intro-frontend/pages/index.tsx
@@ -4,21 +4,31 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import AddressForm from "../components/AddressForm";
-import * as web3 from "@solana/web3.js";
+import * as Web3 from "@solana/web3.js";
 
 const Home: NextPage = () => {
   const [balance, setBalance] = useState(0);
   const [address, setAddress] = useState("");
+  const [isExecutable, setIsExecutable] = useState(false);
 
   const addressSubmittedHandler = (address: string) => {
-    const key = new web3.PublicKey(address);
-    setAddress(key.toBase58());
+    try {
+      setAddress(address)
+      const key = new Web3.PublicKey(address)
+      const connection = new Web3.Connection(Web3.clusterApiUrl('devnet'))
+      
+      connection.getBalance(key).then(balance => {
+        setBalance(balance / Web3.LAMPORTS_PER_SOL)
+      })
 
-    const connection = new web3.Connection(web3.clusterApiUrl("devnet"));
-
-    connection.getBalance(key).then((balance) => {
-      setBalance(balance / web3.LAMPORTS_PER_SOL);
-    });
+      connection.getAccountInfo(key).then(info => {
+        setIsExecutable(info?.executable ?? false);
+      })
+    } catch (error) {
+      setAddress('')
+      setBalance(0)
+      alert(error)
+    }
   };
 
   return (
@@ -28,6 +38,7 @@ const Home: NextPage = () => {
         <AddressForm handler={addressSubmittedHandler} />
         <p>{`Address: ${address}`}</p>
         <p>{`Balance: ${balance} SOL`}</p>
+        <p>{`Is it executable? ${isExecutable ? 'Yep' : 'Nope'}`}</p>
       </header>
     </div>
   );
