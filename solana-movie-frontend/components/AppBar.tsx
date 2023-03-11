200890fc60def7d1162bfbdac4e91e93edf67962
muffafa <mm.savar@gmail.com>
2023-03-11 03:08:44 +0300

    dynamic button added to handle w hydrition failed

--- solana-movie-frontend/components/AppBar.tsx
+++ solana-movie-frontend/components/AppBar.tsx
@@ -1,14 +1,19 @@
 import { FC } from 'react'
 import styles from '../styles/Home.module.css'
-import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
 import Image from 'next/image'
+import dynamic from 'next/dynamic'
+
+const WalletMultiButtonDynamic = dynamic(
+    async () => (await import('@solana/wallet-adapter-react-ui')).WalletMultiButton,
+    { ssr: false }
+);
 
 export const AppBar: FC = () => {
     return (
         <div className={styles.AppHeader}>
-            <Image src="/solanaLogo.png" height={30} width={200} />
+            <Image src="/solanaLogo.png" height={30} width={200} alt={''} />
             <span>Movie Reviews</span>
-            <WalletMultiButton />
+            <WalletMultiButtonDynamic />
         </div>
     )
 }
--- solana-send-sol-frontend/components/AppBar.tsx
+++ solana-send-sol-frontend/components/AppBar.tsx
@@ -1,15 +1,19 @@
 import { FC } from 'react'
 import styles from '../styles/Home.module.css'
 import Image from 'next/image'
-import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
+import dynamic from 'next/dynamic'
 
+const WalletMultiButtonDynamic = dynamic(
+    async () => (await import('@solana/wallet-adapter-react-ui')).WalletMultiButton,
+    { ssr: false }
+);
 
 export const AppBar: FC = () => {
     return (
         <div className={styles.AppHeader}>
             <Image src="/solanaLogo.png" height={30} width={200} alt={''} />
             <span>Wallet-Adapter Example</span>
-            <WalletMultiButton />
+            <WalletMultiButtonDynamic />
         </div>
     )
 }
